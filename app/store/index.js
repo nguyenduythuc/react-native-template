3861d367bb7867620889178cd8f3cbfc767282d7
Nguyen Duy Thuc <nguyenduythuc@MacBook.local>
2018-12-27 17:54:13 +0700

    reformat code

--- app/store/index.js
+++ app/store/index.js
@@ -1,35 +1,38 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import { connect } from 'react-redux'
-import { persistStore, persistCombineReducers } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { connect } from 'react-redux';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import createSagaMiddleware from 'redux-saga';
 import {
   createReactNavigationReduxMiddleware,
   reduxifyNavigator
 } from 'react-navigation-redux-helpers';
-import rootReducer from '../reducers'
-import rootSaga from '../sagas'
-import AppNavigator from '../navigations'
+import rootReducer from '../reducers';
+import rootSaga from '../sagas';
+import AppNavigator from '../navigations';
 
 const persistConfig = {
   key: 'root',
   storage,
   whitelist: ['debug'],
   blacklist: ['navState', 'form']
-}
+};
 
-const persistedReducer = persistCombineReducers(persistConfig, rootReducer)
-const sagaMiddleware = createSagaMiddleware()
+const persistedReducer = persistCombineReducers(persistConfig, rootReducer);
+const sagaMiddleware = createSagaMiddleware();
 const navMiddleware = createReactNavigationReduxMiddleware(
   'template',
-  state => state.navState,
+  state => state.navState
 );
 
 export function configStore() {
-  const store = createStore(persistedReducer, compose(applyMiddleware(sagaMiddleware, navMiddleware)))
-  sagaMiddleware.run(rootSaga)
-  const persistor = persistStore(store)
-  return { store, persistor }
+  const store = createStore(
+    persistedReducer,
+    compose(applyMiddleware(sagaMiddleware, navMiddleware))
+  );
+  sagaMiddleware.run(rootSaga);
+  const persistor = persistStore(store);
+  return { store, persistor };
 }
 
 const App = reduxifyNavigator(AppNavigator, 'template');
